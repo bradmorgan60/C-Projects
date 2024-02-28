66b7f446a7ee0a5d1f71d95c31518f44e3a0d031
Brad Morgan <jbradmorg16@gmail.com>
2024-02-28 06:41:23 -0500

    first commit

---
+++ Digital_Clock/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    int hour, minute, second;
+    hour = minute = second = 0;
+
+    while(1) {
+        system("clear");
+        printf("%02d : %02d : %02d", hour, minute, second);
+
+        fflush(stdout);
+        second++;
+
+        if (second == 60)
+        {
+            minute += 1;
+            second = 0;
+        }
+        else if (minute == 60) {
+            hour += 1;
+            minute = 0;
+        }
+        else if (hour == 12) {
+            hour = 1;
+            minute = second = 0;
+        }
+        sleep(1);
+
+    }
+
+    return 0;
+
+}
+
+void EnterTime(int hour, int minute) {
+    int hour, minute;
+    printf("Enter the hour: ");
+    scanf("%d", &hour);
+
+    printf("Enter the minute: ");
+    scanf("%d", &minute);
+}
